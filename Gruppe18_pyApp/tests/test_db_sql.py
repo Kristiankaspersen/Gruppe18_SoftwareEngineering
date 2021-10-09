2e4811a0c5abdbc6ce5271640fee2aa2f94a9002
hardstein <89636138+hardstein@users.noreply.github.com>
2021-10-09 20:15:05 +0200

    Laget noen flere tester mot db

--- Gruppe18_pyApp/tests/test_db_sql.py
+++ Gruppe18_pyApp/tests/test_db_sql.py
@@ -9,12 +9,12 @@ def setup_database():
     cursor = con.cursor()
     cursor.execute('''
 	    CREATE TABLE users
-        (name text, type text)''')
+        (name text, type text, mail text)''')
     sample_users = [
-        ('Per', 'Seller'),
-        ('Arne', 'Buyer'),
+        ('Per', 'Seller', 'per@test.com'),
+        ('Arne', 'Buyer', 'arne1@grp18.no'),
     ]
-    cursor.executemany('INSERT INTO users VALUES(?, ?)', sample_users)
+    cursor.executemany('INSERT INTO users VALUES(?, ?, ?)', sample_users)
     yield con
 
 
@@ -24,10 +24,45 @@ def test_connection(setup_database):
     assert len(list(cursor.execute('SELECT * FROM users'))) == 2
 
 
-def test_add_to_db(setup_database):
-    # Test to add to the db
+def test_add_to_table(setup_database):
+    # Test to add to the table
     cursor = setup_database
-    cursor.execute('''INSERT INTO users VALUES('Mona', 'Buyer')''')
+    cursor.execute('''INSERT INTO users VALUES('Mona', 'Buyer', 'mona.lisa@gmail.com')''')
     assert len(list(cursor.execute('SELECT * FROM users'))) == 3
 
+
+def test_remove_from_table(setup_database):
+    # Test to remove from table
+    cursor = setup_database
+    cursor.execute('''DELETE FROM users WHERE name = "Mona" ''')
+    assert len(list(cursor.execute('SELECT * FROM users'))) == 2
+
+
+def test_for_mail_already_in_use(setup_database):
+    # Testing if there are more than one of the mail in the table
+    cursor = setup_database
+    mail = "Epost@test.com"
+    cursor.execute('''INSERT INTO users VALUES('Epost', 'Buyer', ?)''', (mail,))
+    # cursor.execute('''INSERT INTO users VALUES('Hei', 'Seller', ?)''', (mail,))
+    assert len(list(cursor.execute("SELECT * FROM users WHERE mail= ?", (mail,)))) == 1
+
+
+def test_for_at_sign_in_mail():
+    # Testing if char (@) exists
+    mail = "Epost@test.com"
+    AT_SIGN = "@"
+    if AT_SIGN in mail:
+        assert True
+    else:
+        assert False
+
+
+def test_if_name_exists(setup_database):
+    # Testing if name already exists
+    # --- Mest for å prøve, kan bygges på videre
+    cursor = setup_database
+    liste = list(cursor.execute("SELECT name FROM users"))
+    name = ('Per',)
+    for i in liste:
+        assert i == name or 'Arne'
 
