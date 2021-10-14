a3aedb398cbedbfaa3dbf0fbff19e459e64123ab
Kristiankaspersen <kristian.kaspersen@hiof.no>
2021-10-15 01:44:21 +0200

    Kristian k: Have connectet the user form in the HTML with the DB in routs. So now it is possible to make a user and store it in the DB. I am missing the validation control

--- Gruppe18_pyApp/app_flask/routes.py
+++ Gruppe18_pyApp/app_flask/routes.py
@@ -2,6 +2,8 @@
 from flask import render_template
 from app_flask import app
 from app_flask.forms import RegisterUserForm
+from app_flask.models import User
+from app_flask import db
 
 @app.route("/")
 @app.route("/home")
@@ -12,8 +14,14 @@ def home_page():
 def test_page():
     return "<h1>Home Page</h1>"
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register_user_page():
     form = RegisterUserForm()
+    if form.validate_on_submit():
+        creating_user_in_db = User(username=form.username.data,
+                                   email=form.email.data,
+                                   password_hash=form.password1.data)
+        db.session.add(creating_user_in_db)
+        db.session.commit()
     return render_template("registerUser.html", form=form)
 
