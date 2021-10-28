1f74a8b99c87d5ed37a00d97b75589cfa5e0fe94
hardstein <89636138+hardstein@users.noreply.github.com>
2021-10-28 13:28:09 +0200

    Update models.py

--- Gruppe18_pyApp/app_flask/models.py
+++ Gruppe18_pyApp/app_flask/models.py
@@ -9,16 +9,18 @@ class User(db.Model):
     password_hash = db.Column(db.String(length=60), nullable=False)
     # Authenticated for the user set False:
     authenticated = db.Column(db.Boolean, default=False)
+
     # adding a relationship between goods and user
-    #goods = db.relationship('goods', backref='goods_owned_by_user', lazy=True)
+    # goods = db.relationship('goods', backref='goods_owned_by_user', lazy=True)
 
     @property
     def password(self):
         return self.password
 
-    #@property.setter
-    #def password(self, plain_password_txt):
+    # @property.setter
+    # def password(self, plain_password_txt):
     #    self.password_hash = bcrypt.generate_password_hash(plain_password_txt).decode("utf-8")
+
 
 #  Need to connect with user
 class Goods(db.Model):
@@ -27,8 +29,9 @@ class Goods(db.Model):
     name = db.Column(db.String(30), unique=True, nullable=False)
     description = db.Column(db.String(70), unique=False, nullable=False)
     price = db.Column(db.Integer, unique=False, nullable=False)
+
     # user.id foregin key in this table, to indicate the owner for the item.
-    #goods_owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    # goods_owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<Goods %r>' % self.name
@@ -39,5 +42,3 @@ def is_the_user_authenticated(self):
     return self.authenticated
 
 # add current_user
-
-
