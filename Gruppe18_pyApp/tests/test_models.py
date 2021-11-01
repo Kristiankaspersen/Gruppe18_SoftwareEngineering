1c12378be770361ffbbf3919f6940668080a85c6
Kristiankaspersen <kristian.kaspersen@hiof.no>
2021-11-01 21:46:30 +0100

    updated tests on User class

--- Gruppe18_pyApp/tests/test_models.py
+++ Gruppe18_pyApp/tests/test_models.py
@@ -4,17 +4,18 @@ from app_flask.models import User, db
 
 @pytest.fixture
 def add_user_in_db():
-    user = User(username="test_user4",
-                email="test_user4@mail.com",
+    user = User(username="test_user",
+                email="test_user@mail.com",
                 password="12345678",
                 profile_type=True
                 )
     db.session.add(user)
     db.session.commit()
-    user = User.query.filter_by(username="test_user4").first()
+    user = User.query.filter_by(username="test_user").first()
     yield user
     db.session.delete(user)
     db.session.commit()
 
-def test_models_create_user_in_db(add_user_db):
-    assert add_user_db
+
+def test_models_create_user_in_db(add_user_in_db):
+    assert add_user_in_db
