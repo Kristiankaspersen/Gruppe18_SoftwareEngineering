18ac9e4e8a99f03df567f7f3cf18e1744caf7729
KRE-95 <68148527+KRE-95@users.noreply.github.com>
2021-11-02 16:12:19 +0500

    I have changed few of the format of the test. Only the client is not config.

--- Gruppe18_pyApp/tests/test_routes.py
+++ Gruppe18_pyApp/tests/test_routes.py
@@ -1,12 +1,15 @@
 import pytest
 from app_flask import app
 
+
 @pytest.fixture
 def client():
     app.config["TESTING"] = True
-    with app.test_client() as client:
+    with app.client() as client:
         yield client
 
+
+""" 
 def test_routes_status_home_page(client):
     assert client.get("/").status_code == 200
     assert client.get("/home").status_code == 200
@@ -15,5 +18,39 @@ def test_routes_status_register_page(client):
     assert client.get("/register").status_code == 200
 
 def test_routes_status_add_goods_page(client):
-    assert client.get("/goods").status_code == 200
+    assert client.get("/goods").status_code == 200"""
 
+# Post and get
+""" config test for Flask app
+    page is requested (get)
+    validate 
+"""
+
+
+# TODO: error not found client
+
+# get
+def test_routes_status_home_page(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    response = client.get('/home')
+    assert response.status_code == 200
+    assert b"Welcome to App" in response.data
+
+
+def test_routes_status_login_get(client):
+    response = client.get('/login')
+    assert response.status_code == 200
+    assert b"you have now logged in" in response.data
+
+
+def test_routes_status_login_post(client):
+    response = client.post('/login')
+    assert response.status_code == 405
+    assert b"You have now logged in" not in response.data
+
+
+def test_routes_status_logout(client):
+    response = client.get('/logout')
+    assert response.status_code == 200
+    assert b"you have now logged out" in response.data
