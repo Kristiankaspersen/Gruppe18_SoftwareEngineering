d1b98a933cbc5a28816f046b52fcc1b12db505e1
KRE-95 <68148527+KRE-95@users.noreply.github.com>
2021-11-02 16:49:12 +0500

    Deleted previous log in&out in route py

--- Gruppe18_pyApp/tests/test_routes.py
+++ Gruppe18_pyApp/tests/test_routes.py
@@ -1,3 +1,4 @@
+import flask
 import pytest
 from app_flask import app
 
@@ -33,24 +34,24 @@ def test_routes_status_add_goods_page(client):
 def test_routes_status_home_page(client):
     response = client.get('/')
     assert response.status_code == 200
-    response = client.get('/home')
+    response = client.get(flask.url_for('index.html'))
     assert response.status_code == 200
     assert b"Welcome to App" in response.data
 
 
 def test_routes_status_login_get(client):
-    response = client.get('/login')
+    response = client.get(flask.url_for('login.html'))
     assert response.status_code == 200
     assert b"you have now logged in" in response.data
 
 
 def test_routes_status_login_post(client):
-    response = client.post('/login')
+    response = client.post(flask.url_for('login.html'))
     assert response.status_code == 405
     assert b"You have now logged in" not in response.data
 
 
 def test_routes_status_logout(client):
-    response = client.get('/logout')
+    response = client.get(flask.url_for('logout.html'))
     assert response.status_code == 200
     assert b"you have now logged out" in response.data
