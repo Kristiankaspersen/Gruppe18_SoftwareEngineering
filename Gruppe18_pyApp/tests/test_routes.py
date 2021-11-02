a1c79c18f1f477f962f34e27e428fc98a4ec2551
KRE-95 <68148527+KRE-95@users.noreply.github.com>
2021-11-02 17:58:22 +0500

    bare gjort om til url

--- Gruppe18_pyApp/tests/test_routes.py
+++ Gruppe18_pyApp/tests/test_routes.py
@@ -9,17 +9,20 @@ def client():
     with app.client() as client:
         yield client
 
+# prøvd test client eller client samme problem
 
-""" 
-def test_routes_status_home_page(client):
-    assert client.get("/").status_code == 200
-    assert client.get("/home").status_code == 200
+    """ 
+def test_routes_status_home_page(test_client):
+    assert test_client.get("/").status_code == 200
+    assert test_client.get("/home").status_code == 200
 
-def test_routes_status_register_page(client):
-    assert client.get("/register").status_code == 200
+def test_routes_status_register_page(test_client):
+    assert test_client.get("/register").status_code == 200
 
-def test_routes_status_add_goods_page(client):
-    assert client.get("/goods").status_code == 200"""
+def test_routes_status_add_goods_page(test_client):
+    assert test_client.get("/goods").status_code == 200 
+    """
+
 
 # Post and get
 """ config test for Flask app
@@ -28,30 +31,27 @@ def test_routes_status_add_goods_page(client):
 """
 
 
-# TODO: error not found client
+# TODO: error not found test_client
 
 # get
-def test_routes_status_home_page(client):
-    response = client.get('/')
+def test_routes_status_home_page(test_client):
+    response = test_client.get('/')
     assert response.status_code == 200
-    response = client.get(flask.url_for('index.html'))
+    response = test_client.get(flask.url_for('index.html'))
     assert response.status_code == 200
     assert b"Welcome to App" in response.data
 
 
-def test_routes_status_login_get(client):
-    response = client.get(flask.url_for('login.html'))
+def test_routes_status_login(test_client):
+    response = test_client.get(flask.url_for('login.html'))
     assert response.status_code == 200
     assert b"you have now logged in" in response.data
-
-
-def test_routes_status_login_post(client):
-    response = client.post(flask.url_for('login.html'))
+    response = test_client.post(flask.url_for('login.html'))
     assert response.status_code == 405
     assert b"You have now logged in" not in response.data
 
 
-def test_routes_status_logout(client):
-    response = client.get(flask.url_for('logout.html'))
+def test_routes_status_logout(test_client):
+    response = test_client.get(flask.url_for('index.html'))
     assert response.status_code == 200
     assert b"you have now logged out" in response.data
