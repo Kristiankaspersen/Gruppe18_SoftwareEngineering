748100db3b91be9ae0d366c9e51b6fa2e97106d9
hardstein <89636138+hardstein@users.noreply.github.com>
2021-11-25 11:59:47 +0100

    gjort endringer på en test i test_app
    tester en model

--- Gruppe18_pyApp/tests/test_app.py
+++ Gruppe18_pyApp/tests/test_app.py
@@ -1,6 +1,5 @@
 import pytest
-from app_flask import app, db
-from app_flask.routes import add_goods_with_parameter
+from app_flask import app
 from app_flask.models import Goods
 
 
@@ -20,26 +19,23 @@ def test_if_app_template_folder_is_named_templates():
     assert app.template_folder == "templates"
 
 
-def test_add_row_to_table_goods():  # Lagd en ny funksjon som tar parametere, testen er altså ikke helt rett
-    add_goods_with_parameter("benk", "deilig og lite brukt", 69, 3)
-    assert Goods.query.filter_by(name='benk').first()
-
-
-def test_delete_row_from_table_goods():
-    assert Goods.query.filter_by(name='benk').delete()
-    db.session.commit()
+# def test_delete_row_from_table_goods():
+#     assert Goods.query.filter_by(name='benk').delete()
+#     db.session.commit()
 
 
 @pytest.fixture(scope='module')
-def new_item_to_goods():
-    benk = Goods(name="benk", description="deilig og lite brukt", price=69, seller_id=3)
+def new_object_of_goods():
+    benk = Goods(name='Benk',
+                 description='Laget i tre',
+                 product_number='123459',
+                 price=50)
     return benk
 
 
-def test_new_item_to_goods(new_item_to_goods):
-    """Testing if Goods model work properly when creating a new 'goods'"""
-    assert new_item_to_goods.name == "benk"
-    assert new_item_to_goods.description == "deilig og lite brukt"
-    assert new_item_to_goods.price == 69
-    assert new_item_to_goods.seller_id == 3
+def test_if_model_goods_can_create_object(new_object_of_goods):
+    """Testing if Goods model name, description and price work properly when creating a new items"""
+    assert new_object_of_goods.name == "Benk"
+    assert new_object_of_goods.description == "Laget i tre"
+    assert new_object_of_goods.price == 50
 
