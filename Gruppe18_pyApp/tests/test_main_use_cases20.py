14b0a2570a2850110420ba12139384ed03472488
Kristiankaspersen <kristian.kaspersen@hiof.no>
2021-12-06 01:54:50 +0100

    Updating, and fixing conflicts, updating with old data, tests for add_auction_item

--- Gruppe18_pyApp/tests/test_main_use_cases20.py
+++ Gruppe18_pyApp/tests/test_main_use_cases20.py
@@ -1,57 +1,61 @@
-from app_flask.main.use_cases import add_auction_item
+from app_flask.main.use_cases import add_auction_item, add_market_item
 from app_flask.models import db, User, Goods, Store
 from app_flask import create_app
-app = create_app()
-ctx = app.app_context()
-ctx.push()
+
 
 
 def test_main_use_cases_add_auction_item(client, existing_store_user):
-    data = {
-    "name": "Test_Samsung",
-    "description": "En samsung 11 som er brukt i 2 år",
-    "price": "1500",
-    "product_number":"000000",
-    "auctionItem": "ItemForAuction",
-    "submit": "Submit+ad",
-    }
 
-    add_auction_item(data["name"], data["description"], data["price"], data["product_number"], existing_store_user.id)
-    auction_item = Goods.query.filter(product_number=data["product_number"])
+    name = "Test_samsung"
+    description = "En samsung 11"
+    price = 1500
+    product_number = "000000"
+    store_owner = existing_store_user.id
+
+    add_auction_item(name, description, price, product_number, store_owner)
+    auction_item = Goods.query.filter_by(name="Test_samsung").first()
+    assert auction_item is not None
+    assert auction_item.goods_type == 1
+    assert auction_item.name == "Test_samsung"
+    assert auction_item.price == 1500
+    assert auction_item.product_number == "000000"
+    assert auction_item.store_owner == store_owner
+    db.session.delete(auction_item)
+    db.session.commit()
+
+def test_main_use_cases_add_goods_item(client, existing_store_user):
+    name = "Test_samsung"
+    description = "En samsung 11"
+    price = 1500
+    product_number = "000000"
+    store_owner = existing_store_user.id
+
+    add_market_item(name, description, price, product_number, store_owner)
+    auction_item = Goods.query.filter_by(name="Test_samsung").first()
+    print(auction_item.goods_type)
     assert auction_item is not None
     assert auction_item.goods_type == 0
-    assert auction_item.name == "Test_Samsung"
+    assert auction_item.name == "Test_samsung"
     assert auction_item.price == 1500
-    assert auction_item.product_number == 000000
-    assert auction_item.store_owner == existing_store_user.id
+    assert auction_item.product_number == "000000"
+    assert auction_item.store_owner == store_owner
+    db.session.delete(auction_item)
+    db.session.commit()
 
-
-
-
-# def add_auction_item(name, description, price, product_number, store_owner):
-#     app = create_app()
-#     ctx = app.app_context()
-#     ctx.push()
-#     new_goods = Goods(
-#         name=name,
-#         description=description,
-#         price=price,
-#         product_number=product_number,
-#         store_owner=store_owner,
-#         goods_type=0
-#     )
-#     db.session.add(new_goods)
-#     db.session.commit()
-#
-#     flash("Item added to the auction market")
-#     ctx.pop()
-#     # here put the item they have put inside
-#     return new_goods
-
-def test_main_use_cases_add_goods_item():
     pass
 
-def test_main_use_cases_buying_product():
+def test_main_use_cases_buying_product_with_enough_money():
+    data = {
+        "name": "Test_Samsung",
+        "description": "En samsung 11 som er brukt i 2 år",
+        "price": "1500",
+        "product_number": "000000",
+        "auctionItem": "ItemForAuction",
+        "submit": "Submit+ad",
+    }
+    pass
+
+def test_main_use_cases_buying_product_not_enough_money():
     pass
 
 def test_main_use_cases_bidding_on_product():
@@ -62,5 +66,3 @@ def test_main_use_cases_accepting_bidding_offer():
 
 def test_main_use_cases_show_current_highest_bidding_offer_in_store():
     pass
-
-ctx.pop()
