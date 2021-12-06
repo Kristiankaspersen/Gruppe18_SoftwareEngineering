a6693d257fb4254b2efe1cb3c04d3fb40207d87e
Kristiankaspersen <kristian.kaspersen@hiof.no>
2021-12-06 15:39:34 +0100

    removed an unneccesary value from parameter in bidding_on_product, refactored the function

--- Gruppe18_pyApp/app_flask/main/use_cases.py
+++ Gruppe18_pyApp/app_flask/main/use_cases.py
@@ -72,13 +72,12 @@ def buying_product(buy_item_product_number, bought_from_store, current_user_id):
 
 
 
-def bidding_on_product(bid_item, bid_from_store, offer, item_id, item_name, user_id, user_name, store_user_id):
+def bidding_on_product(bid_item_product_number, offer, item_id, item_name, user_id, user_name, store_user_id):
     app = create_app()
     ctx = app.app_context()
     ctx.push()
 
-    item_bidded_on = Goods.query.filter_by(product_number=bid_item).first()
-    store_bidding_from = User.query.filter_by(id=bid_from_store).first()
+    item_bidded_on = Goods.query.filter_by(product_number=bid_item_product_number).first()
 
     if offer >= item_bidded_on.price + 10:  # current_price:
         item_bidded_on.price = offer
