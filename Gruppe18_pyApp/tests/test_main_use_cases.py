12375d5027d339b96947951d34e04ba3dcb45553
Kristiankaspersen <kristian.kaspersen@hiof.no>
2021-12-07 23:16:00 +0100

    Have cleaned the use-cases tests

--- Gruppe18_pyApp/tests/test_main_use_cases.py
+++ Gruppe18_pyApp/tests/test_main_use_cases.py
@@ -1,7 +1,6 @@
 from app_flask.main.use_cases import add_auction_item, add_market_item, \
     buying_product, bidding_on_product, accepting_bidding_offer, show_current_highest_bidding_offer_in_store
 from app_flask.models import db, User, Goods, Store, Bidding
-from app_flask import create_app
 
 
 # TC - 023
@@ -25,19 +24,9 @@ def test_main_use_cases_add_auction_item(client, existing_store_user):
     db.session.delete(auction_item)
     db.session.commit()
 
-# def test_main_use_cases_add_auction_item2(add_goods_item_function):
-#     assert add_goods_item_function is not None
-#     assert add_goods_item_function.goods_type == 0
-#     assert add_goods_item_function.name == "Test_samsung"
-#     assert add_goods_item_function.price == 1500
-#     assert add_goods_item_function.product_number == "000000"
-#     assert add_goods_item_function.store_owner ==
-
-#
 
 # TC-024
 def test_main_use_cases_add_goods_item(client, existing_store_user):
-    # Må endre på disse testene, de trenger fixture.
     name = "Test_samsung"
     description = "En samsung 11"
     price = 1500
@@ -69,7 +58,7 @@ def test_main_use_cases_buying_product_with_enough_money(client, existing_store_
     bool_value = buying_product(buy_item_product_number, bought_from_store, current_user_id)
     assert bool_value == True
 
-# TC-025 - kun test med ulik variabel
+# TC-025
 def test_main_use_cases_buying_product_not_enough_money(client, existing_store_user, existing_user, existing_item_in_market):
     current_user_id = existing_user.id
     buy_item_product_number = existing_item_in_market.product_number
@@ -81,7 +70,6 @@ def test_main_use_cases_buying_product_not_enough_money(client, existing_store_u
 
 # TC-026
 def test_main_use_cases_bidding_on_product_with_enough_money(existing_user, existing_store_user, existing_item_in_auction, delete_test_bidding_item):
-    # data bid_item_product_number, offer, item_id, item_name, user_id, user_name, store_user_id
     bid_item_product_number = existing_item_in_auction.product_number
     offer = 811
     item_id = existing_item_in_auction.id
@@ -95,7 +83,7 @@ def test_main_use_cases_bidding_on_product_with_enough_money(existing_user, exis
 # TC-026 ( samme , kun med ulik variabel)
 def test_main_use_cases_bidding_on_product_not_enough_money(existing_user, existing_store_user,
                                                              existing_item_in_auction):
-    # data bid_item_product_number, offer, item_id, item_name, user_id, user_name, store_user_id
+
     bid_item_product_number = existing_item_in_auction.product_number
     offer = 0
     item_id = existing_item_in_auction.id
@@ -108,7 +96,6 @@ def test_main_use_cases_bidding_on_product_not_enough_money(existing_user, exist
                                     store_user_id)
     assert bool_value == False
 
-    # Tanker, vil jeg ha en product_number også i bidding item.
 
 # TC- 027
 def test_main_use_cases_accepting_bidding_offer_have_enough_cash(existing_user,existing_store_user,existing_item_in_auction):
@@ -120,7 +107,7 @@ def test_main_use_cases_accepting_bidding_offer_have_enough_cash(existing_user,e
     bool_value = accepting_bidding_offer(accept_item_id, accept_from_user_id, current_user_store_id)
 
     assert bool_value is True
-# TC- 027 ( ulik variabel)
+# TC- 027
 def test_main_use_cases_accepting_bidding_offer_have_not_enough_cash(existing_user,existing_store_user,existing_item_in_auction):
 
     accept_item_id = existing_item_in_auction.id
